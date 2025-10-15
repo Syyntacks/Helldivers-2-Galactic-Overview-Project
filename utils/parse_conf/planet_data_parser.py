1bdde33eb49e3cd05863d1604e89a4cb21395be7
Syyntacks <anthonydamaso8@gmail.com>
2025-10-14 21:38:58 -0400

    V0.1.6 - Beginning to accumulate sources for the planet_data_parser.py | Removed unused file | Created virtual environment for lib requirements

--- utils/parse_conf/planet_data_parser.py
+++ utils/parse_conf/planet_data_parser.py
@@ -1,16 +1,17 @@
 import json
 from typing import Dict, Any, Union, List
-from data_fetcher import fetch_data_from_url
+from utils.parse_conf.data_fetcher import fetch_data_from_url
 from conf import settings
 
 PLANETS_URL: str = settings.url.get("planets")
 PLANET_DETAILS_URL: str = settings.url.get("planet_stats")
-PLANET_STATS_URL: str = settings.url.get("status")
+PLANET_STATUS_URL: str = settings.url.get("status")
 
-class PlanetParser:
+class PlanetParser():
     """
         Handles all functions regarding all planets
         on the Galactic Map.
+        \n3 sources combined.
     """
 
     def __init__(self):
@@ -19,38 +20,54 @@ class PlanetParser:
     
 
     def _fetch_and_combine(self):
-        # Creates one dictionary from both API endpoints
+        # Creates one dictionary from API endpoints
         planets_data = fetch_data_from_url(PLANETS_URL)
         details_data = fetch_data_from_url(PLANET_DETAILS_URL)
-        stats_data = fetch_data_from_url(PLANET_STATS_URL)
+        status_data = fetch_data_from_url(PLANET_STATUS_URL)
 
-        if not planets_data or not details_data or not stats_data:
+        if not planets_data or not details_data or not status_data:
             print("\nCould not combine data due to fetching errors.")
             return
         
-        # Dictionary Comprehension 
+        # Dictionary Comprehension --> for further reference
         planets_dict = {planet['index']: planet for planet in planets_data}
         details_dict = {detail['planetIndex']: detail for detail in details_data}
-        stats_dict = {stat['statIndex']: stat for stat in stats_data}
+        status_planets_dict = {status['statusIndex']: status for status in status_data}
 
         # Combine data
         for index, planet_detail in planets_dict.items():
             details = details_dict.get(index, {}) # Get planet details
-            stats = stats_dict.get(index, {}) # Get stats of planet
+            status = status_planets_dict.get(index, {}) # Get stats of planet
 
             # Labelling
             self.combined_data[index] = {
-                # Planet Details
+                # planets
                 'index': index,
-                'name': planet_detail.get('name', 'Unknown'),
-                'sector': planet_detail.get('sector', 'Unknown Sector'),
-                'type': planet_detail.get('type', 'Unknown Type'),
-                'biome': planet_detail.get('biome', {}).get('name', 'Unknown Biome'),
+                'name': details.get('name', 'Unknown'),
+                'sector': details.get('sector', 'Unknown Sector'),
+                'type': details.get('type', 'Unknown Type'),
+                'biome': details.get('biome', {}).get('name', 'Unknown Biome'),
                 'description': planet_detail.get('biome', {}).get('description', 'No Description'),
                 'environName': [env.get('name') for env in planet_detail.get('environmentals', [])],
                 'environDesc': [env.get('description') for env in planet_detail.get('environmentals', [])],
                 'weatherEffects': planet_detail.get('weather_effects'),
                 
-                # Stats
-                'players': stats
+                # status
+                'players': status.get('players', 0),
+                'owner': status.get('owner', 'N/A'),
+
+                # planet_stats/planets_stats
+                "missionsWon": "Missions Won",
+                "missionsLost": "Missions Lost",
+                "missionTime": "Total Mission Time (ms)",
+                "bugKills": "Terminid Kills",
+                "automatonKills": "Automaton Kills",
+                "illuminateKills": "Illuminate Kills",
+                "bulletsFired": "Total Bullets Fired",
+                "bulletsHit": "Total Bullets Hit",
+                "timePlayed": "Total Time Played (ms)",
+                "deaths": "Total Deaths",
+                "friendlies": "Friendly Kills",
+                "missionSuccessRate": "Mission Success Rate",
+
             }
--- utils/parse_conf/se_news_parser.py
+++
@@ -1,4 +0,0 @@
-import json
-from . import api_endpoints
-
-## Will handle the parsing of Super Earth's News Messages.
